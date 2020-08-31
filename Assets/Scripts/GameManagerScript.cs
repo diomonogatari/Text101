2dcc1b0c260ea0103e8ef4b3fcceeaefc5692624
Diogo Rosa Carvalho <diodude.lol@gmail.com>
2020-08-31 23:43:38 +0100

    Placeholder text, basic UI

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public Text txtOption1;
+    public Text txtOption2;
+    public Text txtStoryText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtStoryText.text = "wow you have a big pp";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
